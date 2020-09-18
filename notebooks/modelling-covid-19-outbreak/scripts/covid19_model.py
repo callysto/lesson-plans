578a7150d79ef6e8342ec1454e84c9b645c8d084
Laura Gutierrez Funderburk <58710704+lgfunderburk@users.noreply.github.com>
2020-09-17 20:15:34 -0700

    comment script functions

--- notebooks/modelling-covid-19-outbreak/scripts/covid19_model.py
+++ notebooks/modelling-covid-19-outbreak/scripts/covid19_model.py
@@ -1,3 +1,7 @@
+# Author: Laura Gutierrez Funderburk
+# Created on: August 2020
+# Last modified on: September 17 2020
+
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
@@ -8,12 +12,36 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
+def rerun_cell( b ):
+    
+    display(Javascript('IPython.notebook.execute_cell_range(IPython.notebook.get_selected_index()+1,\
+    IPython.notebook.get_selected_index()+2)'))   
 
 # The SEIR model differential equations.
 def deriv(y, t, Delta, beta, mu, epsilon,gamma,alpha,delta):
     
     """
-    
+     This function contains a system of equations for the S.E.I.R. model assuming non constant population
+     death (natural and due infection) and birth rates, as well as reinfection post recovery. 
+
+    Args:
+        y (array): contains five floating point numbers S0, E0, I0, R0, D0 where each denotes initial conditions (float)
+        t (float): variable denoting time 
+        Delta (float): rate of birth
+        beta (float): rate of contact with infectious
+        mu (float): rate of natural death
+        epsilon (float): rate of infectiousness
+        gamma (float): rate of recovery
+        alpha (float): rate of death due disease
+        delta (float): rate of reintegration into susceptible state
+
+    Returns:
+        [dS, dE, dI, dR, dD] (array)
+        dS: differential equation for Susceptible
+        dE: differential equation of Exposed
+        dI: differential equation for Infectious
+        dR: differential equation for Recovered
+        dD: differential equation for Deaths
     """
     
     S, E, I, R, D = y
@@ -26,14 +54,27 @@ def deriv(y, t, Delta, beta, mu, epsilon,gamma,alpha,delta):
     
     return [dS,dE, dI, dR, dD]
 
-
-def plot_infections(Delta, beta, mu, epsilon,gamma,alpha,delta):
-    """
+def run_seir_model(Delta, beta, mu, epsilon,gamma,alpha,delta):
     
     """
+    This function creates an interactive plot simulating the S.E.I.R. model
+    Note that susceptible has been commented out for the Callysto teacher and student notebooks
     
+    Args:
+        Delta (float): rate of birth
+        beta (float): rate of contact with infectious
+        mu (float): rate of natural death
+        epsilon (float): rate of infectiousness
+        gamma (float): rate of recovery
+        alpha (float): rate of death due disease
+        delta (float): rate of reintegration into susceptible state
+        
+    Returns:
+        seir_simulation (pandas DataFrame): contains data resulting from our model for each of the SEIRD stages
+    
+    """
     # Initial number of infected and recovered individuals, I0 and R0.
-    S0, E0,I0, R0 ,D0 = 3700,0,100,0,0
+    S0, E0,I0, R0 ,D0 = 37000,0,1,0,0
     # Total population, N.
     N = S0 + E0 + I0 + R0
     # Initial conditions vector
@@ -42,8 +83,34 @@ def plot_infections(Delta, beta, mu, epsilon,gamma,alpha,delta):
     ret = odeint(deriv, y0, t, args=(Delta, beta, mu, epsilon,gamma,alpha,delta))
     S, E,I, R, D = ret.T
 
-    seir_simulation = pd.DataFrame({"Susceptible":S,"Exposed":E,"Infected":I,"Recovered":R,"Deaths":D, "Time (days)":t})
+    # Build dataframe with the data from the model
+    seir_simulation = pd.DataFrame({"Susceptible":S,"Exposed":E,"Infectious":I,"Recovered":R,"Deaths":D, "Time (days)":t})
 
+    return seir_simulation
+def plot_infections(Delta, beta, mu, epsilon,gamma,alpha,delta):
+    
+    """
+    This function creates an interactive plot simulating the S.E.I.R. model
+    Note that susceptible has been commented out for the Callysto teacher and student notebooks
+    
+    Args:
+        Delta (float): rate of birth
+        beta (float): rate of contact with infectious
+        mu (float): rate of natural death
+        epsilon (float): rate of infectiousness
+        gamma (float): rate of recovery
+        alpha (float): rate of death due disease
+        delta (float): rate of reintegration into susceptible state
+        
+    Returns:
+        None
+    
+    """
+    
+    # Run simulation
+    seir_simulation = run_seir_model(Delta, beta, mu, epsilon,gamma,alpha,delta)
+    
+    # Create layout
     layout = dict( xaxis=dict(title='Time (days)', linecolor='#d9d9d9', mirror=True),
               yaxis=dict(title='Number of people', linecolor='#d9d9d9', mirror=True))
     
@@ -57,9 +124,9 @@ def plot_infections(Delta, beta, mu, epsilon,gamma,alpha,delta):
                         mode='lines',
                         name='Exposed'))
     
-    fig.add_trace(go.Scatter(x=seir_simulation["Time (days)"], y=seir_simulation["Infected"],
+    fig.add_trace(go.Scatter(x=seir_simulation["Time (days)"], y=seir_simulation["Infectious"],
                     mode='lines',
-                    name='Infected'))
+                    name='Infectious'))
     
     fig.add_trace(go.Scatter(x=seir_simulation["Time (days)"], y=seir_simulation["Recovered"],
                         mode='lines', name='Recovered'))
@@ -73,6 +140,17 @@ def plot_infections(Delta, beta, mu, epsilon,gamma,alpha,delta):
     
 
 def tinker_beta(beta):
+    """
+    This function creates an interactive plot simulating the S.E.I.R. model
+      that allows the user to modify the beta parameter only, while leaving the remaining parameters
+      in the model constant
+    
+    Args:
+        beta (float): rate of contact with infectious
+        
+    Returns:
+        None
+    """
     epsilon = 0.1
     alpha = 0.005
     gamma = 0.1
@@ -86,6 +164,18 @@ def tinker_beta(beta):
     
     
 def tinker_beta_alpha(beta,alpha):
+    """
+    This function creates an interactive plot simulating the S.E.I.R. model
+      that allows the user to modify the beta and alpha parameters only, while leaving the remaining parameters
+      in the model constant
+    
+    Args:
+        beta (float): rate of contact with infectious
+        alpha (float): rate of death due disease
+        
+    Returns:
+        None
+    """
     epsilon = 0.1
     gamma = 0.1
     mu = 0
@@ -97,11 +187,21 @@ def tinker_beta_alpha(beta,alpha):
     plot_infections(Delta, beta, mu, epsilon, gamma, alpha,delta)
     
     
-def plot_model(b):
+def plot_model(button):
     
     """
+    This function creates an interactive plot simulating the S.E.I.R. model
+    all parameters for model (excluding initial conditions) are provided by the user
+    via the use of ipywidgets (float sliders)
     
+    Args:
+        button (ipywidgets button)
+        
+    Returns:
+        None
     """
+    
+    # Obtain positional value for parameters from all_the_widget list (see main program for details)
     beta = all_the_widgets[0].value
     eps = all_the_widgets[1].value
     gamma = all_the_widgets[2].value
@@ -109,11 +209,14 @@ def plot_model(b):
     Delta =  all_the_widgets[4].value
     mu =  all_the_widgets[5].value
     delta =  all_the_widgets[6].value
+    # Compute R0 
     numerator = beta*eps
-   
     denominator = (alpha + gamma + mu)*(eps + mu)
+    # Clear plot for next use
     clear_output()
+    # Display user menu
     display(tab)
+    # Plot simumation
     print("Each infection will generate approximately", numerator/denominator , "new infections.")
     plot_infections(Delta, beta, mu, eps,gamma,alpha,delta)
     
@@ -121,10 +224,13 @@ def plot_model(b):
     
 if __name__ == "__main__":
     
-    style = {'description_width': 'initial'}
+    
     # A grid of time points (in days)
     t = np.linspace(0, 750, 750)
-
+    
+    # Widget style
+    style = {'description_width': 'initial'}
+   
     # Create interactive menu with parameters
     all_the_widgets = [widgets.FloatSlider(
                             min=0, max=1, step=0.01, value=0.5,style =style,description='Beta: contact rate'),
@@ -161,6 +267,47 @@ if __name__ == "__main__":
     tab.set_title(0, 'Choose Parameters')
     
     clear_output()
+    
+    
+     # Question 0
+    student_text0 = widgets.Textarea( value='', 
+                                     placeholder='Write your answer here. Press Record Answer when you finish.', 
+                                     description='', disabled=False , layout=Layout(width='100%', height='75px') )
+    student_button0 = widgets.Button(button_style='info',
+                                     description="Record Answer", 
+                                     layout=Layout(width='15%', height='30px'))
+
+    student_button0.on_click( rerun_cell ) 
+    
+    # Question 1
+    student_text1 = widgets.Textarea( value='', 
+                                     placeholder='Write your answer here. Press Record Answer when you finish.', 
+                                     description='', disabled=False , layout=Layout(width='100%', height='75px') )
+    student_button1 = widgets.Button(button_style='info',
+                                     description="Record Answer", 
+                                     layout=Layout(width='15%', height='30px'))
+
+    student_button1.on_click( rerun_cell ) 
+    
+    # Question2 
+    student_text2 = widgets.Textarea( value='', 
+                                     placeholder='Write your answer here. Press Record Answer when you finish.', 
+                                     description='', disabled=False , layout=Layout(width='100%', height='75px') )
+    student_button2 = widgets.Button(button_style='info',
+                                     description="Record Answer", 
+                                     layout=Layout(width='15%', height='30px'))
+
+    student_button2.on_click( rerun_cell ) 
+    
+    # Question 3
+    student_text3 = widgets.Textarea( value='', 
+                                     placeholder='Write your answer here. Press Record Answer when you finish.', 
+                                     description='', disabled=False , layout=Layout(width='100%', height='75px') )
+    student_button3 = widgets.Button(button_style='info',
+                                     description="Record Answer", 
+                                     layout=Layout(width='15%', height='30px'))
+
+    student_button3.on_click( rerun_cell ) 
     
     
     
--- notebooks/modelling-covid-19-outbreak/scripts/open_data.py
+++ notebooks/modelling-covid-19-outbreak/scripts/open_data.py
@@ -1,9 +1,101 @@
-import requests as r
+# Author: Laura Gutierrez Funderburk
+# Created on: August 2020
+# Last modified on: September 17 2020
+
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+from ipywidgets import interact, interact_manual, widgets, Layout, VBox, HBox, Button
+from IPython.display import display, Javascript, Markdown, HTML, clear_output
 import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go
+import requests as r
+
+
+# The SEIR model differential equations.
+def deriv(y, t, Delta, beta, mu, epsilon,gamma,alpha,delta):
+    
+    """
+     This function contains a system of equations for the S.E.I.R. model assuming non constant population
+     death (natural and due infection) and birth rates, as well as reinfection post recovery. 
+
+    Args:
+        y (array): contains five floating point numbers S0, E0, I0, R0, D0 where each denotes initial conditions (float)
+        t (float): variable denoting time 
+        Delta (float): rate of birth
+        beta (float): rate of contact with infectious
+        mu (float): rate of natural death
+        epsilon (float): rate of infectiousness
+        gamma (float): rate of recovery
+        alpha (float): rate of death due disease
+        delta (float): rate of reintegration into susceptible state
+
+    Returns:
+        [dS, dE, dI, dR, dD] (array)
+        dS: differential equation for Susceptible
+        dE: differential equation of Exposed
+        dI: differential equation for Infectious
+        dR: differential equation for Recovered
+        dD: differential equation for Deaths
+    """
+    
+    S, E, I, R, D = y
+    N = S + E + I + R
+    dS = Delta*N  - beta*S*I/N - mu*S + delta*R
+    dE = beta*S*I/N - (mu + epsilon)*E
+    dI = epsilon*E - (gamma + mu + alpha)*I
+    dR = gamma*I - mu*R - delta*R
+    dD = alpha*I 
+    
+    return [dS,dE, dI, dR, dD]
+
+def run_seir_model(Delta, beta, mu, epsilon,gamma,alpha,delta):
+    
+    """
+    This function creates an interactive plot simulating the S.E.I.R. model
+    Note that susceptible has been commented out for the Callysto teacher and student notebooks
+    
+    Args:
+        Delta (float): rate of birth
+        beta (float): rate of contact with infectious
+        mu (float): rate of natural death
+        epsilon (float): rate of infectiousness
+        gamma (float): rate of recovery
+        alpha (float): rate of death due disease
+        delta (float): rate of reintegration into susceptible state
+        
+    Returns:
+        seir_simulation (pandas DataFrame): contains data resulting from our model for each of the SEIRD stages
+    
+    """
+    # Initial number of infected and recovered individuals, I0 and R0.
+    S0, E0,I0, R0 ,D0 = 37000,0,1,0,0
+    # Total population, N.
+    N = S0 + E0 + I0 + R0
+    # Initial conditions vector
+    y0 = S0,E0, I0, R0, D0
+    # Integrate the SIR equations over the time grid, t.
+    ret = odeint(deriv, y0, t, args=(Delta, beta, mu, epsilon,gamma,alpha,delta))
+    S, E,I, R, D = ret.T
+
+    # Build dataframe with the data from the model
+    seir_simulation = pd.DataFrame({"Susceptible":S,"Exposed":E,"Infectious":I,"Recovered":R,"Deaths":D, "Time (days)":t})
+
+    return seir_simulation
 
 # Define a function to drop the history.prefix
 # Create function drop_prefix
 def drop_prefix(self, prefix):
+    """
+    This function applies and renames str.lstrip method to a pandas dataframe column 
+    
+    Args:
+        prefix (str): name of column to apply function to
+        
+    Returns:
+        self: renamed column
+    """
     self.columns = self.columns.str.lstrip(prefix)
     return self
 
@@ -11,7 +103,15 @@ def drop_prefix(self, prefix):
 
 # Define function which removes history. prefix, and orders the column dates in ascending order
 def order_dates(flat_df):
-
+    """
+    This function parses and cleans up dataframe obtained from requesting COVID19 data
+    
+    Args:
+        flat_df (DataFrame object): dataframe containing COVID 19 data from Hopkins database
+        
+    Returns:
+        final (DataFrame object): cleaned up dataframe
+    """
     # Drop prefix
     flat_df.drop_prefix('history.')
     flat_df.drop_prefix("coordinates.")
@@ -30,20 +130,31 @@ def order_dates(flat_df):
 
 def fit_data(Delta, beta, mu, epsilon,gamma,alpha,delta):
 
+    """
+    This plots the actual number of reported cases of COVID19 against our simulation (infectious stage)
     
-    S0, E0,I0, R0,D0 = 3700000,0,1,0,0
-    # Total population, N.
-    N = S0 + E0 + I0 + R0
-    # Initial conditions vector
-    y0 = [S0,E0, I0, R0,D0]
-    ret = odeint(deriv, y0, t, args=(Delta, beta, mu, epsilon,gamma,alpha,delta))
-    S, E,I, R ,D= ret.T
-    
-    seir_simulation = pd.DataFrame({"Susceptible":S,"Exposed":E,"Infectious":I,"Recovered":R,"Time (days)":t})
+    Args:
+        Delta (float): rate of birth
+        beta (float): rate of contact with infectious
+        mu (float): rate of natural death
+        epsilon (float): rate of infectiousness
+        gamma (float): rate of recovery
+        alpha (float): rate of death due disease
+        delta (float): rate of reintegration into susceptible state
+        
+    Returns:
+        None
+    """
+    # Run simulation
+    seir_simulation = run_seir_model(Delta, beta, mu, epsilon,gamma,alpha,delta)
+    # Assign a date column
     seir_simulation['date'] = pd.date_range(start='01/24/2020', periods=len(seir_simulation), freq='D')
     
+    # Create scatter plot for real covid 19 data
     trace3 = go.Scatter(x = non_cumulative_cases.index,y=non_cumulative_cases["TotalDailyCase"])
+    # Create scatter plot for simulation (infectious stage)
     trace2 = go.Scatter(x = seir_simulation["date"],y=seir_simulation["Infectious"],yaxis='y2')
+    # Create layout
     layout = go.Layout(
         title= ('First guess to fit model: infectious against number of reported cases in ' + str(country)),
         yaxis=dict(title='Daily Number of  Reported Infections',\
@@ -51,15 +162,24 @@ def fit_data(Delta, beta, mu, epsilon,gamma,alpha,delta):
             yaxis2=dict(title='Number of infectious members (our model)', titlefont=dict(color='red'), \
                         tickfont=dict(color='red'), overlaying='y', side='right'),
             showlegend=False)
+    # Create a single figure with both scatter plots
     fig = go.Figure(data=[trace3,trace2],layout=layout)
     
     fig.show()
 
-def plot_model_and_data(b):
+def plot_model_and_data(button):
+    """
+    This function creates an interactive plot with a first guess for parameters in our SEIR model
+    when compared against real number of reported COVID 19 cases
     
+    Args:
+        button (ipywidgets button)
+        
+    Returns:
+        None
     """
     
-    """
+    # Obtain positional value for parameters from all_the_widget list (see main program for details)
     beta = all_the_widgets[0].value
     eps = all_the_widgets[1].value
     gamma = all_the_widgets[2].value
@@ -67,12 +187,15 @@ def plot_model_and_data(b):
     Delta =  all_the_widgets[4].value
     mu =  all_the_widgets[5].value
     delta =  all_the_widgets[6].value
+    # Compute R0 
     numerator = beta*eps
     denominator = (alpha + gamma + mu)*(eps + mu)
+    # Clear plot for next use
     clear_output()
+    # Display user menu
     display(tab1)
+    # Plot simumation
     print("Each infection will generate approximately", numerator/denominator , "new infections.")
-    
     fit_data(Delta, beta, mu, eps,gamma,alpha,delta)
     
 if __name__ == "__main__":
