4fcc8bbcb335808189f6650aa82b72ff550a58d2
Michael Lamoureux <mlamoureux@users.noreply.github.com>
2024-01-04 21:43:17 -0800

    Update ph5auto.js, with async and await
    onAttach function needs to be asynchronous, and setDataInterval needs await.

--- notebooks/plants/ph5auto.js
+++ notebooks/plants/ph5auto.js
@@ -180,10 +180,10 @@ export function render({ model, el }) {
             button.innerHTML = `Click to connect`;        
         }
     };
-    function onAttach(ch) {
+    function async onAttach(ch) {
         console.log('channel is attached' + ch);
         textStatus.innerHTML = 'Attached.  ';
-        ch.setDataInterval(2000);
+        await ch.setDataInterval(2000);
     }
     async function closeUSB() {
         console.log('closeUSB called');
